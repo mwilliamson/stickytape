b3e6686b6d018843bdd42f3a4c3cff72009fa0b1
Michael Williamson <mike@zwobble.org>
2013-03-09 11:30:28 +0000

    Simplify imports in prelude.py by making them local to functions

--- stickytape/prelude.py
+++ stickytape/prelude.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python
 
-import tempfile as __stickytape_tempfile
+
 import contextlib as __stickytape_contextlib
-import shutil as __stickytape_shutil
 
 @__stickytape_contextlib.contextmanager
 def __stickytape_temporary_dir():
-    dir_path = __stickytape_tempfile.mkdtemp()
+    import tempfile
+    import shutil
+    dir_path = tempfile.mkdtemp()
     try:
         yield dir_path
     finally:
-        __stickytape_shutil.rmtree(dir_path)
+        shutil.rmtree(dir_path)
 
 with __stickytape_temporary_dir() as __stickytape_working_dir:
     def __stickytape_write_module(path, contents):
@@ -31,6 +32,6 @@ with __stickytape_temporary_dir() as __stickytape_working_dir:
         with open(full_path, "w") as module_file:
             module_file.write(contents)
 
-    import sys as __stickytape_sys
-    __stickytape_sys.path.insert(0, __stickytape_working_dir)
+    import sys
+    sys.path.insert(0, __stickytape_working_dir)
 
