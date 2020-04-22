8d71d7e124fb8d6f89001f32d4cc15d4ef1c6f5f
Michael Williamson <mike@zwobble.org>
2020-04-22 21:51:10 +0100

    Update supported Python versions

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ setup(
     author_email='mike@zwobble.org',
     url='http://github.com/mwilliamson/stickytape',
     packages=['stickytape'],
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
     entry_points={
         "console_scripts": [
             "stickytape=stickytape.main:main"
