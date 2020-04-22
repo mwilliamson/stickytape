0c4b7f8bfca54994729930b04c99917a655ba68c
Michael Williamson <mike@zwobble.org>
2020-04-22 21:51:33 +0100

    Add license to setup.py

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@ setup(
     url='http://github.com/mwilliamson/stickytape',
     packages=['stickytape'],
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
+    license="BSD-2-Clause",
     entry_points={
         "console_scripts": [
             "stickytape=stickytape.main:main"
