4e40e0351e90cc3de112b00bb1db73367d2adb36
Michael Williamson <mike@zwobble.org>
2021-01-29 21:33:19 +0000

    Bump version to 0.2.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 setup(
     name='stickytape',
-    version='0.2.0',
+    version='0.2.1',
     description='Convert Python packages into a single script',
     long_description=read("README.rst"),
     author='Michael Williamson',
